c7360d03acb8f6d513db03845e0b0a396f1426f2
prajwalsbeta <thelolthelolthelol19@gmail.com>
2020-10-01 19:54:18 +0530

    Time Delay Functionality Added a user input to add delay between messages

--- whattsappautomate.py
+++ whattsappautomate.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com')
-# To get Whattsapp URL 
+# To get Whattsapp URL
 
-name=input("Enter the name of user or group : ")
-msg=input("Enter your Message :")
-count=int(input("Enter the Count How many times you want to send msg :"))
+name = input("Enter the name of user or group : ")
+msg = input("Enter your Message :")
+count = int(input("Enter the Count How many times you want to send msg :"))
+delay = int(input("Enter the delay between the messages to be sent  in seconds:"))
 
 input("Enter anything after Scanning The QR Code")
 
-user=driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 
-msg_box=driver.find_element_by_class_name('_3uMse')
+msg_box = driver.find_element_by_class_name('_3uMse')
 
 for i in range(count):
     msg_box.send_keys(msg)
-    btn=driver.find_element_by_class_name('_1U1xa')
+    btn = driver.find_element_by_class_name('_1U1xa')
     btn.click()
-
+    time.sleep(delay)
